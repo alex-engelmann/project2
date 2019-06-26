7150010378c99fd063598c65b25caa39fb79d6be
Jaison <illbeheer@users.noreply.github.com>
2019-06-25 21:15:54 -0700

    yello

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,22 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.high_scoresDB.findAll({}).then(function(dbhigh_scoresDB) {
+      res.json(dbhigh_scoresDB);
     });
   });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    db.high_scoresDB.create(req.body).then(function(dbhigh_scoresDB) {
+      res.json(dbhigh_scoresDB);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.high_scoresDB.destroy({ where: { id: req.params.id } }).then(function(dbhigh_scoresDB) {
+      res.json(dbhigh_scoresDB);
     });
   });
 };
