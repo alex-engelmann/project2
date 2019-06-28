bc5feed3957ff7895bbb8635b46e4c9c172d7eb2
Jaison <illbeheer@users.noreply.github.com>
2019-06-27 19:26:11 -0700

    commit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,27 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.high_scoresDB.findAll({}).then(function(dbhigh_scoresDB) {
+  app.get("/api/examples", function (req, res) {
+    db.high_scoresDB.findAll({}).then(function (dbhigh_scoresDB) {
       res.json(dbhigh_scoresDB);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.high_scoresDB.create(req.body).then(function(dbhigh_scoresDB) {
+  app.post("/api/score", function (req, res) {
+    db.high_scoresDB.create(req.body).then(function (dbhigh_scoresDB) {
       res.json(dbhigh_scoresDB);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.high_scoresDB.destroy({ where: { id: req.params.id } }).then(function(dbhigh_scoresDB) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.high_scoresDB.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbhigh_scoresDB) {
       res.json(dbhigh_scoresDB);
     });
   });
