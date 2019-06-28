c26a6cddda34c631a8a12d27edf1f6ef1bde9c1f
Kim Helen Quach <KimHelenQuach@campus-103-083.ucdavis.edu>
2019-06-27 20:58:24 -0700

    kelen's sprites

--- models/schema.sql
+++ models/schema.sql
@@ -1,3 +1,15 @@
 DROP DATABASE IF EXISTS high_scoresDB;
 CREATE DATABASE high_scoresDB;
+USE high_scoresDB
 
+CREATE TABLE scores
+(
+    id INTEGER
+    AUTO_INCREMENT,
+    name VARCHAR
+    (30),
+score INTEGER,
+created DATE,
+PRIMARY KEY
+    (id)
+); 
