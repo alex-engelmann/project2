eccc78b7a0db030f301bbf0479df833ef84b76aa
Alex Engelmann <alex.engelmann@gmail.com>
2019-07-05 19:31:49 -0700

    heroku fix

--- models/example.js
+++ models/example.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var Score = sequelize.define("Score", {
     name: {
       type: DataTypes.STRING,
-      notNull: true},
+      allowNull: false},
     score: {
       type: DataTypes.STRING
     }
